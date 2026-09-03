4eb1947a9b6ccd8c8dbe8c0bb97e0e64a68d673f
HEMANTH <hemanthannamneedi05@gmail.com>
2026-09-03 11:20:24 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,73 @@
+
+name = "Hemanth"
+age = 21
+print(f"hello im {name} and im {age} years old!")
+
+
+
+# 10 small programs using variables (swap numbers, sum, average, simple interest, etc.).
+
+# 1- sum of two numbers
+a = 10
+b = 20
+sum = a+b
+print("sum is a+b:", sum)
+
+# 2- swaping two numbers
+a = 10
+b = 20
+a,b = b,a
+print("after swapping:")
+print("a=",a)
+print("b=",b)
+
+# 3-average of three numbers
+c = 30
+avg = (a+b+c)/3
+print("average=",avg)
+
+# 4-simple interest
+p = 1000
+t = 2
+r = 5
+si = (p*t*r)/100
+print("Simple Interest:",si)
+
+# 5-area of rectangle
+len = 10
+width = 3
+area = len * width
+print("Area of rectangle:",area)
+
+# 6-calculating total and percentage
+maths = 58
+science = 34
+english = 87
+total = maths + science + english
+percentage = total/3
+print("total marks:",total)
+print("percentage:",percentage)
+
+# 7-even or odd
+num = 34
+if num % 2 == 0:
+    print("number is even")
+else:
+    print("number is odd")
+
+# 8-celsius to fahrenheit
+cel = 49
+fah = (cel * 9/5) + 32
+print("fahrenheit=",fah)
+
+# 9-calculate age
+birth_year = 2005
+curr_yr = 2026
+agee = curr_yr - birth_year
+print("Present age is:",agee)
+
+# 10-calculate total bill
+price = 5000
+quantity = 4
+amount = price * quantity
+print("total bill:",amount)
