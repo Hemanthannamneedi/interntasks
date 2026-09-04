c337f1595f8c5475db7a91a6d7280cc9a0d26387
HEMANTH <hemanthannamneedi05@gmail.com>
2026-09-04 12:38:51 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,23 @@
+# Number classification program.
+# for loop
+for i in range(1,11):
+    print(i)
+# while loop
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+
+
+
+# pattern program using star
+for i in range(1,6):
+    print("*" * i)
+
+
+# pattern program using numbers
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end="")
+    print()
