9c84ad6d866bc56372959b4bbc74a627a7bb2fa7
HEMANTH <hemanthannamneedi05@gmail.com>
2026-09-15 15:01:32 +0530

    Update day8.py

--- day8.py
+++ day8.py
@@ -1,27 +1,27 @@
 # Coding Task:
 
-# contacts = {"Hemanth":"9703388399",
-#             "Priyanka":"7638745283",
-#             "Ganesh":"9820843792"}
-# print("Contacts: ")
-# for name, phone in contacts.items():
-#     print(name, ":", phone)
+contacts = {"Hemanth":"9703388399",
+            "Priyanka":"7638745283",
+            "Ganesh":"9820843792"}
+print("Contacts: ")
+for name, phone in contacts.items():
+    print(name, ":", phone)
 
 # Assignment:
-# contacts = {"Hemanth":"9703388399",
-#             "Priyanka":"7638745283",
-#             "Ganesh":"9820843792"}
-# name = input("Enter a name: ")
-# phone = contacts.get(name)
-# if phone:
-#     print("Phno: ",phone)
-# else:
-#     print("Contact not found.")
+contacts = {"Hemanth":"9703388399",
+            "Priyanka":"7638745283",
+            "Ganesh":"9820843792"}
+name = input("Enter a name: ")
+phone = contacts.get(name)
+if phone:
+    print("Phno: ",phone)
+else:
+    print("Contact not found.")
 
 # set operations:
-# set1 = {"Apple", "Banana", "Mango"}
-# set2 = {"Banana", "Orange", "Mango"}
-#
-# print("Union:", set1.union(set2))
-# print("Intersection:", set1.intersection(set2))
-# print("Difference:", set1.difference(set2))
+set1 = {"Apple", "Banana", "Mango"}
+set2 = {"Banana", "Orange", "Mango"}
+
+print("Union:", set1.union(set2))
+print("Intersection:", set1.intersection(set2))
+print("Difference:", set1.difference(set2))
