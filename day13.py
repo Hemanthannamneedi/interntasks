51c6d2bfb1d3856dff52cede8b0c58097803bfaf
HEMANTH <hemanthannamneedi05@gmail.com>
2026-09-15 14:58:17 +0530

    Update day13.py

--- day13.py
+++ day13.py
@@ -1,39 +1,39 @@
 # Coding Task:
-# import json
-#
-# class Student:
-#     def __init__(self, student_id, name, marks):
-#         self.student_id = student_id
-#         self.name = name
-#         self.marks = marks
-#
-#     def display(self):
-#         print("ID:", self.student_id)
-#         print("Name:", self.name)
-#         print("Marks:", self.marks)
-#
-# student = Student(101, "Hemanth", 85)
-#
-# # Save student to JSON
-# student_data = {
-#     "student_id": student.student_id,
-#     "name": student.name,
-#     "marks": student.marks
-# }
-#
-# with open("student.json", "w") as file:
-#     json.dump(student_data, file, indent=4)
-#
-# print("Student data saved successfully.")
-#
-# # Read student from JSON
-# with open("student.json", "r") as file:
-#     data = json.load(file)
-#
-# print("\nStudent details:")
-# print("ID:", data["student_id"])
-# print("Name:", data["name"])
-# print("Marks:", data["marks"])
+import json
+
+class Student:
+    def __init__(self, student_id, name, marks):
+        self.student_id = student_id
+        self.name = name
+        self.marks = marks
+
+    def display(self):
+        print("ID:", self.student_id)
+        print("Name:", self.name)
+        print("Marks:", self.marks)
+
+student = Student(101, "Hemanth", 85)
+
+# Save student to JSON
+student_data = {
+    "student_id": student.student_id,
+    "name": student.name,
+    "marks": student.marks
+}
+
+with open("student.json", "w") as file:
+    json.dump(student_data, file, indent=4)
+
+print("Student data saved successfully.")
+
+# Read student from JSON
+with open("student.json", "r") as file:
+    data = json.load(file)
+
+print("\nStudent details:")
+print("ID:", data["student_id"])
+print("Name:", data["name"])
+print("Marks:", data["marks"])
 
 # Assignment:
 
